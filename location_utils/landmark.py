b948929b11d55659a4a615003975e1dc788f057d
zhehan1 <154448408+zhehan1@users.noreply.github.com>
2025-07-10 21:43:06 +0800

    Update landmark.py

--- location_utils/landmark.py
+++ location_utils/landmark.py
@@ -1,6 +1,6 @@
 # location_utils/landmark.py
 import logging
-from typing import Optional，Tuple，Union  
+from typing import Optional，Tuple, Union  
 import streamlit as st
 import requests
 from transformers import CLIPProcessor, CLIPModel
